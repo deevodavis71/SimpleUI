39a645d54cf081c3a359f4a221526682f226ccb4
deevodavis71 <deevodavis71@gmail.com>
2017-02-20 15:18:17 +0000

    Initial Commit

---
+++ SimpleUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SimpleUI.Startup))]
+namespace SimpleUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
